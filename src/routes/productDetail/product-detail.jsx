f6491e33bc996f1bba2c7d3eb076fb313509f66f
Palmmyeiei <137021707+Palmmyeiei@users.noreply.github.com>
2024-11-03 14:25:57 +0700

    Update product-detail.jsx

--- src/routes/productDetail/product-detail.jsx
+++ src/routes/productDetail/product-detail.jsx
@@ -28,10 +28,6 @@ export default function ProductDetail() {
   const [product, setProduct] = useState(null); // Initialize product to null
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  useEffect(() => {
-    document.title = "Product Detail"; // Set the page title here
-  }, []);
   
   useEffect(() => {
     const fetchProduct = async () => {
