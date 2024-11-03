fdeb64b5d9e84bc5baa6185f01a770620b763932
Palmmyeiei <137021707+Palmmyeiei@users.noreply.github.com>
2024-11-03 14:26:17 +0700

    Update show-product-detail.jsx

--- src/routes/productDetail/show-product-detail.jsx
+++ src/routes/productDetail/show-product-detail.jsx
@@ -2,8 +2,13 @@ import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
 import ProductDetail from "./product-detail";
 import ProductRecommendations from "./product-recommend";
+import { useEffect } from "react";
 
 function ShowProductDetail() {
+  useEffect(() => {
+    document.title = "Product Detail"; // Set the page title here
+  }, []);
+  
   return (
     <>
       <Navbar />
