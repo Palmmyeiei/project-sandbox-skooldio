d0d5cbd5c34c45bf1810b75fa338a0cec8e31d6c
Palmmyeiei <137021707+Palmmyeiei@users.noreply.github.com>
2024-11-03 14:24:33 +0700

    Update Cart.jsx

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -3,12 +3,15 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import ShoppingCart from "../components/shoppingcart";
 import ProductRecommendations from "./productDetail/product-recommend";
+import { useEffect } from "react";
 
 // import { useCart } from "./cart/card-context"; // Adjust the import path if needed
 // import { Typography, List, ListItem, ListItemText } from "@mui/material";
 
 export default function Cart() {
-  // const { cartItems } = useCart();
+    useEffect(() => {
+    document.title = "Cart"; // Set the page title here
+  }, []);
 
   return (
     <>
